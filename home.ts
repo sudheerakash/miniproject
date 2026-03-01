661a9fde8605f3c4044e3899709afa5768706c40
sudheerakash <sudheerakash07@gmail.com>
2026-03-01 17:00:34 +0530

    Add HomeComponent with donor list retrieval

---
+++ home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class HomeComponent {
+
+  donors = JSON.parse(localStorage.getItem('donorList') || '[]');
+
+}
